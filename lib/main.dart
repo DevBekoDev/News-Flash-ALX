e6f0bda2fc6a8d0f36dd2e5728a7aff5bf511a6d
DevBekoDev <48620096+DevBekoDev@users.noreply.github.com>
2024-08-20 19:46:11 +0300

    set up Apprwite and initialize it

--- lib/main.dart
+++ lib/main.dart
@@ -8,26 +8,31 @@ import 'package:news_flash/screens/settings_screen.dart';
 import 'package:news_flash/screens/sports_news.dart';
 import 'package:news_flash/screens/tech_news.dart';
 import 'package:news_flash/screens/world_news.dart';
+import 'package:appwrite/appwrite.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
+  MyApp({super.key});
+  final Client client = Client()
+      .setEndpoint('https://cloud.appwrite.io/v1')
+      .setProject('66c4c619000805580048');
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  Client client = Client();
+
   // This widget is the root of your application.
   final List<Widget> _childeren = [
     const WorldNews(),
     const SportsNews(),
     const ScienceNews(),
     const TechNews(),
-    const SettingsScreen(),
+    const SettingsScreen()
   ];
 
   NewsViewModel newsViewModel = NewsViewModel();
