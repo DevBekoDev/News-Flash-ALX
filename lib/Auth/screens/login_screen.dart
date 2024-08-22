b41b9de793f5235426cd3303dd2991ec4cc9f056
DevBekoDev <48620096+DevBekoDev@users.noreply.github.com>
2024-08-22 21:01:44 +0300

    login screen design: changed the textfields into textformfields so i can validate both email and password also added the ability to show and hide password

--- lib/Auth/screens/login_screen.dart
+++ lib/Auth/screens/login_screen.dart
@@ -9,66 +9,120 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  // Regex for email validation
+  String? _validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your email';
+    }
+    String pattern = r'\w+@\w+\.\w+';
+    RegExp regex = RegExp(pattern);
+    if (!regex.hasMatch(value)) {
+      return 'Enter a valid email address';
+    }
+    return null;
+  }
+
+  // Password validation
+  String? _validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your password';
+    }
+    if (value.length < 6) {
+      return 'Password must be at least 6 characters';
+    }
+    return null;
+  }
+
+  // Submit function
+  void _submit() {
+    if (_formKey.currentState!.validate()) {
+      //TODO: Login process
+    }
+  }
+
+  //show password
+  IconData hide = CupertinoIcons.eye_slash;
+  IconData show = CupertinoIcons.eye;
+
+  bool visible = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.only(top: 200, left: 40, right: 40),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              TextField(
-                controller: emailController,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20)),
-                ),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              TextField(
-                controller: emailController,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20)),
-                ),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              SizedBox(
-                  width: 400,
-                  child: ElevatedButton.icon(
-                      style: ButtonStyle(
-                          backgroundColor: WidgetStateProperty.all<Color>(
-                              const Color.fromARGB(255, 199, 193, 174))),
-                      onPressed: () {},
-                      label: const Text(
-                        'Sign In',
-                        style: TextStyle(color: Colors.black, fontSize: 18),
+          padding: const EdgeInsets.only(top: 250, right: 40, left: 40),
+          child: Column(children: [
+            Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      suffixIcon: const Icon(Icons.email),
+                      labelText: 'Email',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
                       ),
-                      icon: const Icon(
-                        Icons.login,
-                        color: Colors.black,
-                      ))),
-              const SizedBox(
-                height: 10,
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    validator: _validateEmail,
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              visible = !visible;
+                            });
+                          },
+                          icon: visible ? Icon(show) : Icon(hide)),
+                      labelText: 'Password',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    obscureText: visible,
+                    validator: _validatePassword,
+                  ),
+                  const SizedBox(height: 16),
+                  ElevatedButton.icon(
+                    style: const ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(
+                            Color.fromARGB(255, 199, 193, 174))),
+                    onPressed: _submit,
+                    icon: const Icon(
+                      Icons.login,
+                      color: Colors.black,
+                    ),
+                    label: const Text(
+                      'Sign In',
+                      style: TextStyle(fontSize: 18, color: Colors.black),
+                    ),
+                  ),
+                ],
               ),
-              TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    'Create a new account',
-                    style: TextStyle(fontSize: 18),
-                  ))
-            ],
-          ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'Create a New Account',
+                  style: TextStyle(
+                      fontSize: 19, color: Color.fromARGB(255, 161, 152, 121)),
+                ))
+          ]),
         ),
       ),
     );
